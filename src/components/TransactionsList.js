7a73b98b9f781fc93ae14befad594d5e14493992
Omar ElKordy <omarelkordy9@gmail.com>
2022-05-09 13:54:28 +0200

    Adding TransactionList

--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -1,9 +1,126 @@
-import React from 'react'
+import React, { Component } from 'react'
+import styled from 'styled-components';
+import axios from 'axios';
 
-const TransactionsList = () => {
-  return (
-    <div>TransactionsList</div>
-  )
+const Transaction = props => (
+  <tr>
+    <td>{props.transaction.from}</td>
+    <td>{props.transaction.to}</td>
+    <td>{props.transaction.amount}</td>
+  </tr>
+)
+
+const Section = styled.section`
+  position: relative;
+  min-height: 50vh;
+  width: 80vw;
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+  justify-content: space-evenly;
+`
+const Title = styled.h1`
+  font-size: ${(props) => props.theme.fontxxl};
+  font-family: "Audiowide";
+  font-weight: 300;
+  position: absolute;
+  top: 1rem;
+  left: 6%;
+  z-index: 5;
+`
+const TableContainer = styled.div`
+overflow-x: auto;
+width: 100%;
+`
+const Table = styled.table`
+border-collapse: collapse;
+background: white;
+border-radius: 10px;
+overflow: hidden;
+width: 85%;
+position: relative;
+`
+const TableHead = styled.thead`
+background-color: yellow;
+color: black;
+display: table-header-group;
+vertical-align: middle;
+text-align: center;
+height: 3em;
+
+tr{
+display: table-row;
+color: black;
+text-align: center;
+justify-content: space-evenly;
+}
+`
+const TableBody = styled.tbody`
+display: table-row-group;
+vertical-align: middle;
+color: black;
+
+tr{
+  display: table-row;
+  text-align: center;
+  cursor: pointer;
+  transition: all 0.3s ease;
+  color: grey;
+  font-weight: 500;
+
+  &:hover{
+    color: black;
+    transition: all 0.3s ease;
+  }
 }
 
-export default TransactionsList
+td{
+  padding: 0.5em
+}
+`
+
+export default class TransactionsList extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {transactions: []};
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:8000/transactions/')
+    .then(response => {
+      this.setState({transactions: response.data})
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+  }
+
+  transactionList() {
+    return this.state.transactions.map(currenttransaction => {
+      return <Transaction transaction={currenttransaction} key={currenttransaction._id}></Transaction> 
+    })
+  }
+
+  render(){
+    return (
+      <Section>
+        <Title>Transactions</Title>
+        <TableContainer>
+          <Table>
+            <TableHead>
+              <tr>
+                <th>From</th>
+                <th>To</th>
+                <th>Amount $</th>
+              </tr>
+            </TableHead>
+            <TableBody>
+              {this.transactionList()}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Section>
+    );
+  }
+}
