17288d852e5a3da59810529b8d62a56059ddfcaa
Omar ElKordy <omarelkordy9@gmail.com>
2022-05-12 12:22:19 +0200

    fixing bugs

--- backend/server.js
+++ backend/server.js
@@ -8,38 +8,38 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 8000;
 
-var corsOptions = {
-  origin: 'https://kordy-sparksbank.herokuapp.com',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
+// var corsOptions = {
+//   origin: 'https://kordy-sparksbank.herokuapp.com',
+//   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+// }
  
-app.get('/transactions', cors(corsOptions), function (req, res, next) {
-  res.json({msg: 'This is CORS-enabled for only example.com.'})
-})
-app.get('/users', cors(corsOptions), function (req, res, next) {
-  res.json({msg: 'This is CORS-enabled for only example.com.'})
-})
+// app.get('/transactions', cors(corsOptions), function (req, res, next) {
+//   res.json({msg: 'This is CORS-enabled for only example.com.'})
+// })
+// app.get('/users', cors(corsOptions), function (req, res, next) {
+//   res.json({msg: 'This is CORS-enabled for only example.com.'})
+// })
  
-app.listen(port, function () {
-  console.log('CORS-enabled web server listening on port 80')
-})
+// app.listen(port, function () {
+//   console.log('CORS-enabled web server listening on port 80')
+// })
 
 
-// app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", "https://sparksbank--kordy.herokuapp.com");
-//   res.setHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
-//   if (req.method == "OPTIONS") {
-//     return res.sendStatus(200);
-//   }
-//   next();
-// });
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://sparksbank--kordy.herokuapp.com");
+  res.setHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
+  if (req.method == "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
 
-// // app.use(cors());
+// app.use(cors());
 
-// const corsOption = {
-//   origin: ['https://sparksbank--kordy.herokuapp.com/'],
-// };
-// app.use(cors(corsOption));
+const corsOption = {
+  origin: ['https://sparksbank--kordy.herokuapp.com'],
+};
+app.use(cors(corsOption));
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
@@ -61,6 +61,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/frontend/build', 'index.html'));
 });
 
-// app.listen(port, () => {
-//     console.log(`Server is running on port: ${port}`);
-// });
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
