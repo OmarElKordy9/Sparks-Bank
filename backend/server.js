76d61a7e6551ae2576735823cdc946cfc8f2c736
Omar ElKordy <omarelkordy9@gmail.com>
2022-05-12 02:29:09 +0200

    fixing bugs

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,12 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 8000;
 
-app.use(cors());
+// app.use(cors());
+
+const corsOption = {
+  origin: ['https://sparksbank--kordy.herokuapp.com/'],
+};
+app.use(cors(corsOption));
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
